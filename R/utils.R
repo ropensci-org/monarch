b47927b0ff74261d1ff5928bb5d13524320f5e03
Steffi LaZerte <steffi@steffi.ca>
2026-03-30 14:54:26 -0500

    Better name handling

--- R/utils.R
+++ R/utils.R
@@ -13,6 +13,14 @@ gh_cache <- memoise::memoise(gh::gh, omit_args = c(".max_rate"))
 
 name_options <- function(names) {
   names <- stringr::str_remove_all(names, "\\.")
-  c(names, stringr::str_remove_all(names, "\\b[A-Z]{1} ")) |>
+  names_split <- stringr::str_split_1(names, " ")
+  if(length(names_split) > 2) {
+    names_split <- names_split[c(1, length(names_split))]
+  }
+  c(names, 
+    stringr::str_remove_all(names, "\\b[A-Z]{1} "), 
+    names_split[2], # Family name only
+    paste(names_split, collapse = " ")
+  ) |>
     unique()
 }
