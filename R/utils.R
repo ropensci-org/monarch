1bee018cfc6aa5f4f6487b7f84342e710a9829d8
Steffi LaZerte <steffi@steffi.ca>
2026-03-31 12:32:10 -0500

    formatting

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,3 @@
-
 check_websites <- function(socials) {
   sapply(socials$value[socials$type == "website"], utils::browseURL)
 }
@@ -14,11 +13,12 @@ gh_cache <- memoise::memoise(gh::gh, omit_args = c(".max_rate"))
 name_options <- function(names) {
   names <- stringr::str_remove_all(names, "\\.")
   names_split <- stringr::str_split_1(names, " ")
-  if(length(names_split) > 2) {
+  if (length(names_split) > 2) {
     names_split <- names_split[c(1, length(names_split))]
   }
-  c(names, 
-    stringr::str_remove_all(names, "\\b[A-Z]{1} "), 
+  c(
+    names,
+    stringr::str_remove_all(names, "\\b[A-Z]{1} "),
     names_split[2], # Family name only
     paste(names_split, collapse = " ")
   ) |>
