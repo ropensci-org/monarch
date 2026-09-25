0b657e3e2ab4a4019cbdaabadc78b8c7d7eaf9ee
Maëlle Salmon <maelle.salmon@yahoo.se>
2026-09-25 08:57:43 +0200

    improve functions

--- R/universe.R
+++ R/universe.R
@@ -1,3 +1,17 @@
+#' Get email from R-universe
+#'
+#' @param name Name of the package contributor
+#' @param package Name of the package. If not present, we'll look for any person
+#' with that name in the R-universe.
+#' @param universe Name of the R-universe.
+#'
+#' @returns
+#' An email
+#'
+#' @export
+#' @examples
+#' email_from_universe("Maëlle Salmon", universe = "ropensci")
+#' email_from_universe("Maelle S Salmon", universe = "ropensci")
 email_from_universe <- function(
   name,
   package = NULL,
@@ -10,11 +24,26 @@ email_from_universe <- function(
     httr2::resp_body_json()
 
   if (!is.null(package)) {
-    package <- purrr::keep(universe, \(x) x[["Package"]] == package)
+    package <- purrr::keep(universe, \(x) x[["Package"]] == package) |>
+      unlist(recursive = FALSE)
   } else {
-    # TODO: add fuzzy string search?
-    package <- purrr::keep(universe, \(x) x[["_maintainer"]][["name"]] == name)
+    package <- purrr::keep(universe, \(x) {
+      x[["_maintainer"]][["name"]] == name
+    }) |>
+      unlist(recursive = FALSE)
+
+    if (is.null(package)) {
+      distances <- purrr::map_dbl(
+        universe,
+        \(x) stringdist::stringdist(x[["_maintainer"]][["name"]], name)
+      )
+      package <- universe[[which(distances == min(distances))[1]]]
+    }
   }
 
-  return(package[[1]]$`_maintainer`$email)
+  data.frame(
+    email = package$`_maintainer`$email,
+    name = package$`_maintainer`$name,
+    package = package$Package
+  )
 }
